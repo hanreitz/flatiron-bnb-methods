cf7b6506f44ac5e1944025d025cb05de5f1a4657
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-05 19:25:33 -0400

    commit tree b4a94eeb8be2c43a85a2a6abe6fe663ea4bfd2df

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,24 @@ class Listing < ActiveRecord::Base
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
-  has_many :guests, :class_name => "User", :through => :reservations
+  has_many :guests, :through => :reservations, :class_name => "User"
   
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  after_save :set_host
+  after_destroy :unset_host
+
+  def set_host
+    host.update(host: true)
+  end
+
+  def unset_host
+    if !host.listings.any?
+      host.update(host: false)
+    end
+  end
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,37 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :is_not_host, :is_available, :in_before_out
+
+  def duration
+    (checkin..checkout).to_a.size - 1
+  end
+
+  def total_price
+    duration * listing.price
+  end
+
+  private
+
+  def is_not_host
+    if listing.host == guest
+      errors.add(:guest_id, "You cannot book your own listing.")
+    end
+  end
+
+  def is_available
+    Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+      date_range = r.checkin..r.checkout
+      if date_range.include?(checkin) || date_range.include?(checkout)
+        errors.add(:guest_id, "Sorry, this listing is not available during the requested stay.")
+      end
+    end
+  end
+
+  def in_before_out
+    if checkin && checkout && checkout <= checkin
+      errors.add(:guest_id, "Check-in must be before check-out.")
+    end
+  end
 end
